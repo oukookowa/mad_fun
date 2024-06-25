567e3ec96872254dee4be7e5ef92279ce41655c8
Ouko Okowa <oukopinto@gmail.com>
2024-06-25 11:54:55 +0200

    Added a script for the mad lib game

--- madlib.py
+++ madlib.py
@@ -6,9 +6,9 @@ verb = str(input("Enter an verb: "))
 pronoun = str(input("Enter a pronoun: "))
 
 if adjective and noun and verb and pronoun:
-        print("He was known for his", adjective, "swimming skills."
-                          " His love for", noun, "outmarched his desire to be a star!"
-                                    " He traversed several continents chasing fame. He almost succeeded." 
-                                              " Sadly, he lost the motivation just when he was supposed to", verb + "." 
-                                                        " It was rumoured that Sara was his poison,", pronoun,
-                                                                  "was the one who lured him to a journey of no retreat. He got lost deep into drugs!")
+    print("He was known for his", adjective, "swimming skills."
+          " His love for", noun, "outmarched his desire to be a star!"
+          " He traversed several continents chasing fame. He almost succeeded." 
+          " Sadly, he lost the motivation just when he was supposed to", verb + "." 
+          " It was rumoured that Sara was his poison,", pronoun,
+          "was the one who lured him to a journey of no retreat. He got lost deep into drugs!")
